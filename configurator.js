feb8942497ca1e6ca50be2112f908bf8996013d7
dp-design-site <danypavlov89@gmail.com>
2025-07-23 13:22:49 +0300

    Update configurator.js

--- configurator.js
+++ configurator.js
@@ -12,8 +12,6 @@ function generateConfig() {
     timestamp: new Date().toISOString()
   };
 
-  document.getElementById('output').innerText = JSON.stringify(config, null, 2);
-
   // Път до изображенията и PDF файла
   const basePath = `img/${configID}`;
 
@@ -21,20 +19,35 @@ function generateConfig() {
     <h3>Преглед на чертеж:</h3>
     <div class="gallery">
       <a href="${basePath}/drawing.pdf" target="_blank">
-        <img src="${basePath}/preview_drawing.png" class="main-image" alt="Преглед на чертеж">
+        <img src="${basePath}/preview_drawing.png" class="drawing-preview">
       </a>
       <div class="thumbnail-row">
         <a href="${basePath}/view1.png" target="_blank">
-          <img src="${basePath}/view1.png" class="thumbnail" alt="Изглед 1">
+          <img src="${basePath}/view1.png" class="thumbnail">
         </a>
         <a href="${basePath}/view2.png" target="_blank">
-          <img src="${basePath}/view2.png" class="thumbnail" alt="Изглед 2">
+          <img src="${basePath}/view2.png" class="thumbnail">
         </a>
       </div>
     </div>
+    <button id="orderButton" class="order-button">Поръчай</button>
+    <div id="orderConfirmation" class="order-confirmation"></div>
+    <footer class="footer">© dp-design.tools – всички права запазени</footer>
   `;
 
   document.getElementById('preview').innerHTML = previewHTML;
+  document.getElementById('output').style.display = 'none';
+
+  // Добавяме обработка за бутона Поръчай
+  setTimeout(() => {
+    const btn = document.getElementById('orderButton');
+    if (btn) {
+      btn.addEventListener('click', () => {
+        document.getElementById('orderConfirmation').innerText =
+          'Благодарим за направената поръчка. Наш колега ще се свърже с вас за потвърждение.';
+      });
+    }
+  }, 50);
 }
 
-// Стиловете ще се добавят към основния HTML
+// Стиловете ще се добавят към основния HTML или CSS файл
